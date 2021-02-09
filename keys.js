b112609ce1af358443fa9c94c31206e4a4a2ff5e
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-09 00:08:37 +0000

    Update keys.js

--- keys.js
+++ keys.js
@@ -27,4 +27,33 @@ window.addEventListener('keyup',onKeyUp)
 return keyModel
 } 
 
-const keyModel=addListeners()
+
+
+
+function InputHandler() {
+	this.down = {};
+	this.pressed = {};
+	// capture key presses
+	var _this = this;
+	document.addEventListener("keydown", function(evt) {
+		_this.down[evt.keyCode] = true;
+    console.log(evt.keyCode)
+	});
+	document.addEventListener("keyup", function(evt) {
+		delete _this.down[evt.keyCode];
+		delete _this.pressed[evt.keyCode];
+	});
+};
+
+InputHandler.prototype.isDown = function(code) {
+	return this.down[code];
+};
+
+InputHandler.prototype.isPressed = function(code) {
+	if (this.pressed[code]) {
+		return false;
+	} else if (this.down[code]) {
+		return this.pressed[code] = true;
+	}
+	return false;
+};
