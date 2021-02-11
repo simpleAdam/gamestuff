f426d32f6112c94586f5d8288a4114ea88a5fd6c
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-11 15:24:48 +0000

    Update entities.js

--- entities.js
+++ entities.js
@@ -34,3 +34,28 @@ function Bullet(x, y, vely, w, h, color) {
 Bullet.prototype.update = function() {
 	this.y += this.vely;
 };
+
+
+
+
+class EntityWithTraits {
+    constructor() {
+        this.pos= new Vec2(0,0);
+        this.vel = new Vec2(0,0);
+        this.size = new Vec2(0,0)
+        
+        this.traits=[];
+    }
+    
+    addTrait(trait) {
+        this.traits.push(trait);
+        this[trait.NAME]=trait;
+        
+    }
+    
+    update(deltaTime) {
+        this.traits.forEach(trait => {
+            trait.update(this, deltaTime);
+        })
+    }
+}
