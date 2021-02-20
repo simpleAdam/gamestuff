2e8659c6976ee1099b4b9d22b9ba5167937bae98
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-02-20 21:18:11 +0000

    Update entities.js

--- entities.js
+++ entities.js
@@ -59,3 +59,36 @@ class EntityWithTraits {
         })
     }
 }
+
+
+
+  function Particle() {
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.vx = Math.random() * 10 - 5;
+    this.vy = Math.random() * 10 - 5;
+    this.gravity = 0;
+    particleIndex++;
+    particles[particleIndex] = this;
+    this.id = particleIndex;
+    this.life = 0;
+    this.maxLife = Math.random() * 30 + 50;
+    this.color = "hsla(" + parseInt(Math.random()*255, 10) + ",100%,50%, 0.2)";
+  }
+  
+  Particle.prototype.draw = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+    
+    if (Math.random() < 0.1) {
+      this.vx = Math.random()*10-5;
+      this.vy = Math.random()*10-5;
+    }
+    this.vy += this.gravity;
+    this.life++;
+    if (this.life > this.maxLife){
+      delete particles[this.id];
+    } 
+    c.fillStyle = this.color;
+    c.fillRect(this.x,this.y,10,10);
+  };
