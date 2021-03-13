ce5385708bb1f3a3f6c6dc260a9b8fa7d1619429
simpleAdam <34377387+simpleAdam@users.noreply.github.com>
2021-03-13 14:28:13 +0000

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,5 +1,5 @@
-let 2DArray=Array.from({length:5}, () => Array.from({length:5}, () => RandInt()) )
+let myArray=Array.from({length:5}, () => Array.from({length:5}, () => RandInt()) )
 
 // a type of transpose
 const transpose = (refArray) => refArray[0].map((_, colIndex) => refArray.map(row => row[colIndex]));
-// transpose(2dArray)
+// transpose(myArray)
